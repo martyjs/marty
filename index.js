85aac2e926145488770ba37dee0670fda1df09cd
James Hollingworth <jamiehollingworth@gmail.com>
2014-11-14 10:58:17 +0000

    Replacing lodash with tinydash

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-var fs = require('fs');
 var _ = require('lodash');
 var create = require('./lib/create');
 var Dispatcher = require('./lib/dispatcher');
-var package = JSON.parse(fs.readFileSync(__dirname + '/package.json'), 'utf-8');
 
 var Marty = _.extend({
-  version: package.version,
   dispatcher: new Dispatcher()
 }, create);
 
