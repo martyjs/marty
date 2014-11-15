aaf6a8d943b372488979133df11d631d3d8fe180
James Hollingworth <jamiehollingworth@gmail.com>
2014-11-15 20:47:51 +0000

    Add TodoMVC example. Closes #5

---
+++ examples/flux-todomvc/js/components/TodoApp.react.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2014, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+/**
+ * This component operates as a "Controller-View".  It listens for changes in
+ * the TodoStore and passes the new data to its children.
+ */
+
+var Marty = require('../marty');
+var Footer = require('./Footer.react');
+var Header = require('./Header.react');
+var MainSection = require('./MainSection.react');
+var React = require('react');
+var TodoStore = require('../stores/TodoStore');
+
+var TodoAppState = Marty.createStateMixin({
+  allTodos: TodoStore,
+  getState: function () {
+    return {
+      areAllComplete: TodoStore.areAllComplete()
+    };
+  }
+});
+
+var TodoApp = React.createClass({
+  mixins: [TodoAppState],
+  render: function() {
+  	return (
+      <div>
+        <Header />
+        <MainSection
+          allTodos={this.state.allTodos}
+          areAllComplete={this.state.areAllComplete}
+        />
+        <Footer allTodos={this.state.allTodos} />
+      </div>
+  	);
+  }
+});
+
+module.exports = TodoApp;
