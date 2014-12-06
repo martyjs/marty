2261952d4c9dc74876d69de7a4804c717ac3801b
James Hollingworth <jamiehollingworth@gmail.com>
2014-12-06 23:03:27 +0000

    Adding require.js example

---
+++ examples/requirejs/main.js
@@ -0,0 +1,33 @@
+require.config({
+  paths: {
+    marty: '../../dist/marty',
+    react: '../../node_modules/react/dist/react'
+  }
+});
+
+require(['react', 'marty'], function (React, Marty) {
+  var Store = Marty.createStore({
+    getInitialState: function () {
+      return [{
+        id: 1,
+        displayName: 'Foo'
+      }, {
+        id: 2,
+        displayName: 'Bar'
+      }]
+    }
+  });
+
+  var App = React.createClass({
+    mixins: [Marty.createStateMixin(Store)],
+    render: function () {
+      return React.createElement("ul", null,
+        Object.keys(this.state).map(function (id) {
+          return React.createElement("li", null, this.state[id].displayName);
+        }, this)
+      );
+    }
+  });
+
+  React.render(React.createElement(App), document.getElementById('app'));
+});
