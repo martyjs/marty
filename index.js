5fbe7c9be8212bdb1490b963a643de7f6cfffa2d
James Hollingworth <jamiehollingworth@gmail.com>
2014-12-06 23:44:41 +0000

    Adding ability to set and clear the state of all stores

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var create = require('./lib/create');
 var _ = require('./lib/utils/tinydash');
+var setState = require('./lib/setState');
+var clearState = require('./lib/clearState');
 var Dispatcher = require('./lib/dispatcher');
 var Diagnostics = require('./lib/diagnostics');
 var ActionPayload = require('./lib/actionPayload');
 var ActionStore = require('./lib/stores/actionsStore');
 
 var Marty = _.extend({
+  setState: setState,
   getAction: getAction,
+  clearState: clearState,
   Diagnostics: Diagnostics,
   ActionPayload: ActionPayload,
   Dispatcher: Dispatcher.getCurrent(),
