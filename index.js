fd114eb634e482a53ac8d5f440cd9897605014f7
James Hollingworth <jamiehollingworth@gmail.com>
2014-12-14 00:11:59 +0000

    moving form queries to fetches

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ var Dispatcher = require('./lib/dispatcher');
 var Diagnostics = require('./lib/diagnostics');
 var ActionPayload = require('./lib/actionPayload');
 var ActionStore = require('./lib/stores/actionsStore');
-var StoreQueryResult = require('./lib/storeQuery/result');
 
 var Marty = _.extend({
   getAction: getAction,
   Diagnostics: Diagnostics,
   ActionPayload: ActionPayload,
-  StoreQueryResult: StoreQueryResult,
   Dispatcher: Dispatcher.getCurrent(),
   Stores: {
     Actions: ActionStore
