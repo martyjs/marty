ea146652afc591e76aa8f783e048eb08b2f71183
James Hollingworth <jamiehollingworth@gmail.com>
2015-01-01 21:13:45 +0000

    Simplify releasing (Closes #47)

---
+++ build/release.sh
@@ -0,0 +1,23 @@
+if [[ -n $(git status --porcelain) ]];
+then
+  echo "repo is dirty. please commit before releasing";
+  exit 1;
+fi
+
+echo "updating version"
+version="$(node build/updateVersion.js)"
+
+echo "building v$version"
+make build
+
+echo "commiting source"
+git add -A
+git commit -m "v$version"
+
+echo "creating tag v$version"
+git tag $version
+git push origin master
+git push origin --tags
+
+echo "publishing to NPM"
+npm publish
