4a7fd1ee30e1c094226cdc5f2567e1c6f0921600
James Hollingworth <jamiehollingworth@gmail.com>
2015-03-19 08:53:27 +0000

    Add Location state source and move server cookie implementation into marty-express

---
+++ dist/node/lib/utils/resolve.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var log = require("../logger");
+var warnings = require("../warnings");
+var getContext = require("./getContext");
+
+function resolve(obj, subject) {
+  var context = getContext(subject);
+
+  if (context) {
+    return context.resolve(obj);
+  }
+
+  if (obj.__isDefaultInstance && warnings.cannotFindContext) {
+    log.warn("Warning: Could not find context in object", obj);
+  }
+
+  return obj;
+}
+
+module.exports = resolve;
