39c2ca7ecc66e86ba25f2c5c3838d0ad8234cd03
James Hollingworth <jamiehollingworth@gmail.com>
2015-03-25 18:11:28 +0000

    Fixing container

--- dist/node/lib/createContainer/createContainer.js
+++ dist/node/lib/createContainer/createContainer.js
@@ -2,7 +2,7 @@
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var React = require("react");
+var React = require("../react");
 var log = require("../logger");
 var _ = require("../utils/mindash");
 var uuid = require("../utils/uuid");
