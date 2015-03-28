31cbd4490b5c67ea082a281819caa033836ed395
James Hollingworth <jamiehollingworth@gmail.com>
2015-03-28 18:23:35 +0000

    v0.9.3

--- dist/node/marty.js
+++ dist/node/marty.js
@@ -20,7 +20,7 @@ function createInstance() {
   return _.extend({
     logger: logger,
     dispose: dispose,
-    version: "0.9.2",
+    version: "0.9.3",
     warnings: warnings,
     dispatcher: Dispatcher,
     diagnostics: Diagnostics,
