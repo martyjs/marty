47e0fbccbbf724959b03ae084603fff94c6a80ae
James Hollingworth <jamiehollingworth@gmail.com>
2015-04-02 11:08:52 +0100

    v0.9.7

--- dist/browser/marty.js
+++ dist/browser/marty.js
@@ -10170,7 +10170,7 @@ function createInstance() {
   return _.extend({
     logger: logger,
     dispose: dispose,
-    version: "0.9.6",
+    version: "0.9.7",
     warnings: warnings,
     dispatcher: Dispatcher,
     diagnostics: Diagnostics,
