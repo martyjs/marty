4d1ba4e083609eea18650acd86638f4ac7fc0885
James Hollingworth <jamiehollingworth@gmail.com>
2015-04-03 19:27:29 +0100

    Move core registration out

--- marty.js
+++ marty.js
@@ -4,41 +4,25 @@ var state = require('./lib/state');
 var create = require('./lib/create');
 var logger = require('marty-core/lib/logger');
 var _ = require('marty-core/lib/utils/mindash');
-var dispose = require('./lib/dispose');
 var classes = require('./lib/classes');
-var warnings = require('marty-core/lib/warnings');
 var Registry = require('./lib/registry');
-var Dispatcher = require('marty-core/lib/dispatcher');
-var Diagnostics = require('marty-core/lib/diagnostics');
-var environment = require('marty-core/lib/environment');
-var EventEmitter = require('events').EventEmitter;
 var renderToString = require('./lib/renderToString');
 var MartyBuilder = require('marty-core/lib/martyBuilder');
 
 var builder = new MartyBuilder('0.9.7');
 
+require('marty-core/register')(builder);
 require('marty-store/register')(builder);
 require('marty-constants/register')(builder);
 
 function createInstance() {
   var marty = builder.build();
 
-
   return _.extend(marty, {
-    logger: logger,
-    dispose: dispose,
-    warnings: warnings,
-    dispatcher: Dispatcher,
-    diagnostics: Diagnostics,
     registry: new Registry(),
-    __events: new EventEmitter(),
     renderToString: renderToString,
     createInstance: createInstance,
-
-    // Legacy
-    Dispatcher: Dispatcher,
-    Diagnostics: Diagnostics
-  }, state, create, classes, environment);
+  }, state, create, classes);
 }
 
 module.exports = createInstance();
